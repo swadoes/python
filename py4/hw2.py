4f73c304c3dbb0fafa4d46d65c0fe903f0a5aaca
swadoes <pokemon21332@mail.ru>
2022-12-26 15:26:35 +0300

    hw4

---
+++ py4/hw2.py
@@ -0,0 +1,12 @@
+num = int(input("Введите число: "))
+i = 2  
+lst = []
+old = num
+while i <= num:
+    if num % i == 0:
+        lst.append(i)
+        num //= i
+        i = 2
+    else:
+        i += 1
+print(f"Простые множители числа {old} приведены в списке: {lst}")
---
+++ py4/hw3.py
@@ -0,0 +1,5 @@
+lst = list(map(int, input("Введите числа через пробел:\n").split()))
+print(f"Исходный список: {lst}")
+new_lst = []
+[new_lst.append(i) for i in lst if i not in new_lst]
+print(f"Список из неповторяющихся элементов: {new_lst}")
